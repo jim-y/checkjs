61621ee29627b5b5a76437ac25ef0d381ace15ed
Attila Kling <jimmyfly10@gmail.com>
2014-09-06 22:02:59 +0200

    Rearrange

--- test/commonjs_test.js
+++ test/commonjs_test.js
@@ -1,8 +1,4 @@
 var check = require('../lib/check'),
 	assert = require('assert');
 
-console.info(new Date());
-console.log(check(new Date()).is.empty());
-//check(0).is.empty().should.be.true;
-			//check([]).is.empty().should.be.true;
-			//check({}).is.empty().should.be.true;
+
