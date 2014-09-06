93f6555edd95a8155935c3c4121467ed04b1f130
Attila Kling <jimmyfly10@gmail.com>
2014-09-07 00:18:24 +0200

    added new specs for exist method

--- test/spec/check_global_spec.js
+++ test/spec/check_global_spec.js
@@ -1,8 +1,23 @@
 /*jshint -W030 */
 
 var check = require('../../lib/check'),
-	should = require('should');
-
+	should = require('should'),
+	assert = require('assert');
+/*
+Cheatsheat:
+	Assert:
+		assert.fail(actual, expected, message, operator)
+		assert(value, message), assert.ok(value, [message])
+		assert.equal(actual, expected, [message])
+		assert.notEqual(actual, expected, [message])
+		assert.deepEqual(actual, expected, [message])
+		assert.notDeepEqual(actual, expected, [message])
+		assert.strictEqual(actual, expected, [message])
+		assert.notStrictEqual(actual, expected, [message])
+		assert.throws(block, [error], [message])
+		assert.doesNotThrow(block, [message])
+		assert.ifError(value)
+ */
 describe('check.js', function() {
 
 	'use strict';
@@ -11,8 +26,9 @@ describe('check.js', function() {
 		it('global {Function} check should exist', function() {
 			should.exist(check);
 		});
-		it('an empty argument-list should return false', function() {
-			check().should.be.false;
+		it('an empty argument-list should initially set the argument to null', function() {
+			should(check()[0]).not.be.ok;
+			assert.equal(check()[0], null);
 		});
 		it('should return an object', function() {
 			check('test').should.be.an.Object;
@@ -37,7 +53,7 @@ describe('check.js', function() {
 	});
 
 	describe('empty', function() {
-		it('should have a method empty', function() {
+		it('should have an empty() method', function() {
 			should.exist(check([]).empty);
 		});
 		it('should return true on a single falsy argument', function() {
@@ -59,6 +75,27 @@ describe('check.js', function() {
 		});
 		it('should return false on multiple truthy arguments', function() {
 		  check(1, 'test', new Date(), true).isAll.empty().should.be.false;
+		});
+	});
+
+	describe('exist', function() {
+		it('check should have exist() method', function() {
+			should.exist(check('test').exist);
+		});
+		it('should return true for an existing single value', function() {
+			var obj = { key: 'value' };
+			check(obj).exist().should.be.true;
+		});
+		it('should return false for a non existing value', function() {
+			var obj = null;
+			check(obj).exist().should.be.false;
+		});
+		it('should return true for multiple existing elements', function() {
+			check([], 1, 'existing').all.exist().should.be.true;
+		});
+		it('should return false for multiple elements, \n' +
+				'where at least one elem is non-existent', function() {
+			check([], 1, 'test', null).all.exist().should.be.false;
 		});
 	});
 });
